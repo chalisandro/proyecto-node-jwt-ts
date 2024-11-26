635ce6874f56fcf9a0f1bd066786c48bbcec6c99
chalisandro <chalisandro@gmail.com>
2024-11-25 21:18:09 -0400

    iniciando proyecto dependencias

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import dotenv  from "dotenv";
+dotenv.config();
+import express  from "express";
+const app = express()
+
+app.use(express.json())
+
+//Rutas
+//autentificacion
+//usuario
+export default app;
